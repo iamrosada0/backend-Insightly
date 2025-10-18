07999d011e3c0e42167ee3d9de8989b994d315c9
iamrosada0 <luisrosada@outlook.com>
2025-10-18 13:12:57 +0100

    feat: register dto

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RegisterDto {
+  @ApiProperty({ example: 'user@example.com', description: 'User email' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'john_doe', description: 'Unique username' })
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @ApiProperty({ example: 'password123', description: 'User password' })
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
