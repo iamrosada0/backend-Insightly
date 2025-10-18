203310f3c95513f54e031d44dc2c5105e774d6b0
iamrosada0 <luisrosada@outlook.com>
2025-10-18 12:39:30 +0100

    fix:  add  controllers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,9 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { UsersModule } from 'src/users/users.module';
+import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
@@ -14,12 +17,13 @@ import { UsersModule } from 'src/users/users.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET'),
+        secret: config.get<string>('JWT_SECRET') || 'defaultSecretKey',
         signOptions: { expiresIn: '1h' },
       }),
     }),
   ],
-  providers: [JwtStrategy],
-  exports: [JwtStrategy],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService, JwtStrategy],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
