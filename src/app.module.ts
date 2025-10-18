ee57c6fa13e5fd9374cc671fcc4f24031c456baf
iamrosada0 <luisrosada@outlook.com>
2025-10-18 13:12:45 +0100

    feat: created all parts for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { FeedbackService } from './feedback/feedback.service';
+import { FeedbackController } from './feedback/feedback.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { FeedbackModule } from './feedback/feedback.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    FeedbackModule,
+  ],
+  controllers: [
+    AppController,
+    AuthController,
+    UsersController,
+    FeedbackController,
+  ],
+  providers: [AppService, AuthService, UsersService, FeedbackService],
 })
 export class AppModule {}
