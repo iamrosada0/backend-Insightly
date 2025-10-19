90ae17d618b83a938d950c57ef88fd2a78a508d7
iamrosada0 <luisrosada@outlook.com>
2025-10-19 11:54:49 +0100

    feat: removed eslint

--- src/common/decorator/user.decorator.ts
+++ src/common/decorator/user.decorator.ts
@@ -1,12 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
+import { RequestWithUser } from '../interfaces/request-with-user.interface';
 
 export const User = createParamDecorator(
   (data: keyof JwtPayload | undefined, ctx: ExecutionContext) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const request = ctx.switchToHttp().getRequest();
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    const user = request.user as JwtPayload;
+    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+    const user = request.user;
     return data ? user?.[data] : user;
   },
 );
